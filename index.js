7c659d1ba031577a1f35033f1e57b631426fcc33
Manoj Naidu <manojnaidu619@gmail.com>
2020-05-16 22:46:11 +0530

    Edited routes

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 var path = require('path')
-const fs = require('fs');
+const fs = require('fs')
+const axios = require('axios')
 
 const app = express()
 const port = 3003
 const filePath = path.join(__dirname, "public", "DownloadData.txt")
+const TrackerFile = path.join(__dirname, "public", "IPtracker.txt")
 
 app.use(express.static('public'))
 
 const ReadAndWriteFile = () => {
     fs.readFile(filePath, function(err, data) {
         if (data) {
-            fs.writeFile(filePath, parseInt(data)+1, function (err) {})   
+            fs.writeFile(filePath, parseInt(data)+1, (err) => {})   
         }
     });
+}
+
+const IPtracker = () => {
+    axios.get("http://ipinfo.io").then((response) => {
+        const ip = response["data"]["ip"] + "\n"
+        fs.appendFile(TrackerFile, ip, (err) => {})
+    })
 }
 
 app.get('/', (req, res) => {
@@ -21,10 +30,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/download', (req, res) => {
+    IPtracker()
     ReadAndWriteFile()
     const filePath = path.join(__dirname + '/cocktail-chief.apk')
     const fileName = "cocktail-chief.apk"
     res.download(filePath, fileName)
 })
 
-app.listen(port, () => console.log("Running!"))
+app.get('/stats', (req, res) => {
+    fs.readFile(filePath, (err, data) => {
+        if (data) res.send(data.toString())
+    })
+})
+
+app.get('/ip', (req, res) => {
+    fs.readFile(TrackerFile, (err, data) => {
+        if(data) res.send(data.toString())
+    })
+})
+
+app.listen(port)
