de08aec8590fd389806fe83bafd7d50c5dae902b
Manoj Naidu <manojnaidu619@gmail.com>
2020-05-16 21:25:19 +0530

    Added download link

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', () => {
-    
+    const button = document.querySelector('.download-div')
+    button.addEventListener('click', () => {
+        location.href = location.href + "download"
+    })
 })
