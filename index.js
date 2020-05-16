3dc43acfda4824df2d771d68096db32f033c3e0b
Manoj Naidu <manojnaidu619@gmail.com>
2020-05-16 23:10:04 +0530

    Edited tracker files

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
 const express = require('express')
 var path = require('path')
-const fs = require('fs')
-const axios = require('axios')
 
 const app = express()
 const port = 3003
-const filePath = path.join(__dirname, "public", "DownloadData.txt")
-const TrackerFile = path.join(__dirname, "public", "IPtracker.txt")
 
 app.use(express.static('public'))
 
-const ReadAndWriteFile = () => {
-    fs.readFile(filePath, function(err, data) {
-        if (data) {
-            fs.writeFile(filePath, parseInt(data)+1, (err) => {})   
-        }
-    });
-}
-
-const IPtracker = () => {
-    axios.get("http://ipinfo.io").then((response) => {
-        const ip = response["data"]["ip"] + "\n"
-        fs.appendFile(TrackerFile, ip, (err) => {})
-    })
-}
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + 'public' + '/index.html'))
-})
-
-app.get('/download', (req, res) => {
-    IPtracker()
-    ReadAndWriteFile()
-    const filePath = path.join(__dirname + '/cocktail-chief.apk')
-    const fileName = "cocktail-chief.apk"
-    res.download(filePath, fileName)
-})
-
-app.get('/stats', (req, res) => {
-    fs.readFile(filePath, (err, data) => {
-        if (data) res.send(data.toString())
-    })
-})
-
-app.get('/ip', (req, res) => {
-    fs.readFile(TrackerFile, (err, data) => {
-        if(data) res.send(data.toString())
-    })
 })
 
 app.listen(port)
--- public/script.js
+++ public/script.js
@@ -2,6 +2,6 @@
 window.addEventListener('load', () => {
     const button = document.querySelector('.download-link')
     button.addEventListener('click', () => {
-        location.href = location.href + "download"
+        location.href = "https://www.dropbox.com/s/etfi47x1xc5rlwl/cocktail-chief.apk?dl=0"
     })
 })
